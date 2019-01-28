583a594314c2fe77f2329de363d9ede1935338f7
zfoteff <zfoteff@zagmail.gonzaga.edu>
2019-01-27 17:55:36 -0800

    C-String Pointer project

---
+++ Comp Sci 122/Project 3/proj3_me.cpp
@@ -0,0 +1,43 @@
+/*
+Class: CPSC 122-01
+Team Member 1: Zac Foteff
+Team Member 2: None
+Submitted by: Zac Foteff
+GU Username: zfoteff
+File Name: proj3.cpp
+Programs take user inputted character lists and calls functions my_strcpy and
+my_strcmp
+
+To Build: g++ proj3.cpp -o proj3
+To Execute: ./proj3
+*/
+
+
+int my_strcmp(char*, char*);
+char* my_spy(char*, char*);
+
+int main(){
+  char* userString1[];
+  char* userString2[];
+
+  cout<< "Enter line of text: ";
+  cin>> userString1[];
+  cout<< "Enter second line of text: ";
+  cin>> userString2[];
+
+
+
+  return 0;
+}
+
+
+
+int my_strcmp(char* str1, char* str2){
+
+
+}
+
+char* my_spy(char* str1, char* str2){
+
+
+}
