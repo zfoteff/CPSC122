8428af5de4b4eb7b3a29e5a79c98ce36b665a3f4
zfoteff <zfoteff@zagmail.gonzaga.edu>
2019-01-28 22:26:43 -0800

    1/28/19

--- Comp Sci 122/Project 3/proj3_me.cpp
+++ Comp Sci 122/Project 3/proj3_me.cpp
@@ -14,7 +14,7 @@ To Execute: ./proj3
 
 
 int my_strcmp(char*, char*);
-char* my_spy(char*, char*);
+char* my_strcpy(char*, char*, int);
 
 int main(){
   char* userString1;
@@ -29,6 +29,9 @@ int main(){
 
 
 
+
+  delete userString1;
+  delete userString2;
   return 0;
 }
 
@@ -36,11 +39,22 @@ int main(){
 
 int my_strcmp(char* str1, char* str2){
 
-  if(str1[])
+  if(str1 == str2){
+    return 0;
+  }
 
+  else if(str1[0] > str2[0]){
+    return -1;
+  }
+
+  else{
+    return 1;
+  }
 }
 
-char* my_strcpy(char* str1, char* str2){
-
+char* my_strcpy(char* str1, char* str2, int size){
+  for(int i = 0; i< size; i++){
+    str2[i] = str[i];
+  }
 
 }
