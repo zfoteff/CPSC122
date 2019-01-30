9af9b4b331f0fb432d89ade9545bdfbd7afbf4a7
zfoteff <zfoteff@zagmail.gonzaga.edu>
2019-01-30 13:16:40 -0800

    Tester Files for each function in the project

---
+++ Comp Sci 122/Project 3/strcmpTest.cpp
@@ -0,0 +1,22 @@
+/*
+Class: CPSC 122-01
+Team Member 1: Zac Foteff
+Team Member 2: None
+Submitted by: Zac Foteff
+GU Username: zfoteff
+File Name: strcmpTest.cpp
+Programs take user inputted character lists
+and calls function my_strcmp
+
+To Build: g++ strcmpTest.cpp -o strcmpTest
+To Execute: ./strcmpTest
+*/
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main(int argc, char* argv[]){
+
+
+}
---
+++ Comp Sci 122/Project 3/strcpyTest.cpp
@@ -0,0 +1,13 @@
+/*
+Class: CPSC 122-01
+Team Member 1: Zac Foteff
+Team Member 2: None
+Submitted by: Zac Foteff
+GU Username: zfoteff
+File Name: strcpyTest.cpp
+Programs take user inputted character lists
+and calls function my_strcpy
+
+To Build: g++ strcmpTest.cpp -o strcpyTest
+To Execute: ./strcpyTest
+*/
