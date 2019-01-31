5eedaff2d742b197f63a463ff851d9ee7e530254
zfoteff <zfoteff@zagmail.gonzaga.edu>
2019-01-30 20:54:52 -0800

    -m

--- Comp Sci 122/Project 3/proj3.cpp
+++ Comp Sci 122/Project 3/proj3.cpp
@@ -21,44 +21,44 @@ post: returns 0 if the c-strings are the same
 */
 
 int my_strcmp(char* str1, char* str2){
-  for(int i = 0; i< strlen(str1); i++){
-    tolower(str1[i]);
-  }
-  for(int i = 0; i< strlen(str2); i++){
-    tolower(str2[i]);
-  }
-  //These two for loops change all characters in the strings to lowercase so they can be compared more easily
+ for(int i = 0; i< strlen(str1); i++){
+  tolower(str1[i]);
+ }
+ for(int i = 0; i< strlen(str2); i++){
+  tolower(str2[i]);
+ }
+ //These two for loops change all characters in the strings to lowercase so they can be compared more easily
 
 
-  if(str1 == str2){
-    return 0;
-  }
+ if(str1 == str2){
+  return 0;
+ }
 
-  //This is to check alphabetical order
-  //Will iterate through each character in each string till it finds a set where one is alphabetically greater
-  else if(str1[0] == str2[0]){
-    for(int i = 1; i< strlen(str1); i++){
-      if(str1[i] < str2[i]){
-        return -1;
-        break;
-      }
-
-      else if(str1[i] > str2[i]){
-        return 1;
-        break;
-      }
-    }
+ //This is to check alphabetical order
+ //Will iterate through each character in each string till it finds a set where one is alphabetically greater
+ else if(str1[0] == str2[0]){
+  for(int i = 1; i< strlen(str1); i++){
+   if(str1[i] < str2[i]){
+    return -1;
+    break;
+   }
   }
+ }
+
+ else if(str1[i] > str2[i]){
+  return 1;
+  break;
+ }
 
   // If the character stored in index 0 of str1 has a value less than the value stored in index 0 of str2, that means it appears first alphabetically
-  else if(str1[0] < str2[0]){
-    return -1;
-  }
+ else if(str1[0] < str2[0]){
+  return -1;
+ }
 
   // If the character stored in index 0 of str1 has a value greater than the value stored in index 0 of str2, that means it appears second alphabetically
-  else if(str1[0] > str2[0]){
-    return 1;
-  }
+ else if(str1[0] > str2[0]){
+  return 1;
+ }
 }
 
 
--- Comp Sci 122/Project 3/strcpyTest.cpp
+++ Comp Sci 122/Project 3/strcpyTest.cpp
@@ -20,14 +20,24 @@ char* my_strcpy(char*, char*);
 
 int main(){
   const int MAX_SIZE = 100;
-
   char* userStr1 = NULL;
   char* userStr2 = NULL;
+
   cout<< "Enter a sentence: ";
   cin.getline(userStr1, MAX_SIZE);
-
   cout<< "Enter another sentence: ";
   cin.getline(userStr2, MAX_SIZE);
+
+  //input validation to make sure that str1 > str2 by at least 1
+  while(strlen(userStr1) < strlen(userStr2)+1){
+    cout<< "Please let sentence one be at least one character longer than character 2"<<endl;
+
+    cout<< "Enter sentence 1: ";
+    cin.getline(userStr1, MAX_SIZE);
+    cout<< "Enter sentence 2: ";
+    cin.getline(userStr2, MAX_SIZE);
+  }
+
 
   char* copy = my_strcpy(userStr1, userStr2);
   cout<< "\nCopied sentence: "<<copy<<endl;
@@ -36,11 +46,16 @@ int main(){
   return 0;
 }
 
+/*
+pre: user passes 2 c-strings
+    str1's length is greater than str2's length by at least once
+post: returns memory address to copied c-string
+*/
 char* my_strcpy(char* str1, char* str2){
   int i;
   for(i = 0; i< strlen(str2); i++){
     str2[i] = str1[i];
   }
-  str2[i] = '\0';
+  str2[i] = '\0'; // adds escape character as soon as the str1 is copied into str2
   return str2;
 }
